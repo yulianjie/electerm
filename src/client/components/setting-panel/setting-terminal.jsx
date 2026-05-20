56e9bf2917b749d04066b457076c2c753351d077
Yu Lianjie <jackyu0915@gmail.com>
2026-05-20 14:38:05 +0800

    feat: MobaXterm-style layout — SFTP on left, terminal on right, with optional local file panel
    - New settings: sftpOnLeft, showLocalFileInSftp
    - Change default sshSftpSplitView to true so new sessions are split by default
    - Add per-tab toggle button in SFTP remote header to show/hide local panel
    - Update split-view e2e test for the new default

--- src/client/components/setting-panel/setting-terminal.jsx
+++ src/client/components/setting-panel/setting-terminal.jsx
@@ -591,6 +591,8 @@ export default class SettingTerminal extends Component {
             'ctrlOrMetaOpenTerminalLink',
             'sftpPathFollowSsh',
             'sshSftpSplitView',
+            'sftpOnLeft',
+            'showLocalFileInSftp',
             'showCmdSuggestions',
             'autoReconnectTerminal'
           ].map(d => this.renderToggle(d))
--- src/client/components/sftp/sftp-entry.jsx
+++ src/client/components/sftp/sftp-entry.jsx
@@ -29,7 +29,7 @@ import fs from '../../common/fs'
 import ListTable from './list-table-ui'
 import deepCopy from 'json-deep-copy'
 import isValidPath from '../../common/is-valid-path'
-import { LoadingOutlined, ReloadOutlined } from '@ant-design/icons'
+import { LoadingOutlined, ReloadOutlined, PicLeftOutlined, PicRightOutlined } from '@ant-design/icons'
 import * as owner from './owner-list'
 import AddressBar from './address-bar'
 import getProxy from '../../common/get-proxy'
@@ -551,6 +551,22 @@ export default class Sftp extends Component {
   shouldRenderRemote = () => {
     const { props } = this
     return props.tab?.host && props.tab?.type !== terminalSerialType
+  }
+
+  getShowLocal = () => {
+    const { tab, config } = this.props
+    if (tab && Object.prototype.hasOwnProperty.call(tab, 'showLocalFileInSftp')) {
+      return !!tab.showLocalFileInSftp
+    }
+    return !!config?.showLocalFileInSftp
+  }
+
+  handleToggleShowLocal = () => {
+    const { tab } = this.props
+    if (!tab) return
+    const nv = !this.getShowLocal()
+    window.store.updateTab(tab.id, { showLocalFileInSftp: nv })
+    window.store.triggerResize()
   }
 
   initLocalAll = () => {
@@ -1169,8 +1185,16 @@ export default class Sftp extends Component {
 
   renderSftpPanelTitle (type, username, host) {
     if (type === typeMap.remote) {
+      const showLocal = this.getShowLocal()
+      const ToggleIcon = showLocal ? PicRightOutlined : PicLeftOutlined
+      const toggleTitle = showLocal ? e('hideLocalFiles') : e('showLocalFiles')
       return (
         <div className='sftp-panel-title pd1t pd1b pd1x alignright'>
+          <ToggleIcon
+            className='mg1r pointer'
+            title={toggleTitle}
+            onClick={this.handleToggleShowLocal}
+          />
           <ReloadOutlined
             className='mg1r pointer'
             onClick={this.handleReloadRemoteSftp}
@@ -1295,6 +1319,15 @@ export default class Sftp extends Component {
           height
         }, width)
       )
+    }
+    const showLocal = this.getShowLocal()
+    if (!showLocal) {
+      return this.renderSection(typeMap.remote, {
+        width,
+        left: 0,
+        top: 0,
+        height
+      }, width)
     }
     return arr.map((t, i) => {
       const style = {
